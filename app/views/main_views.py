1fd23c16a942fbca7338e3c1b24e3b1b8b7f0cc6
SeongJoo Kim <castle5314@naver.com>
2023-07-31 22:27:25 +0900

    change: Change db structure and related code

--- app/views/main_views.py
+++ app/views/main_views.py
@@ -9,21 +9,33 @@ from app import models
 bp = Blueprint('main', __name__, url_prefix='/')
 
 
+# noinspection SqlResolve
 @bp.route('/')
 def index():
-    data = models.PatrolStudent.query.all()
-    data.sort(key=lambda x: x.day)
+    # data = models.PatrolStudent.query.all()
+    # data.sort(key=lambda x: x.day)
+    #
+    # lunch1 = ['점심']
+    # smoking1 = ['흡연']
+    #
+    # for i in data:
+    #     if i.type == "점심":
+    #         lunch1.append(i.name)
+    #     else:
+    #         smoking1.append(i.name)
+    #
+    # patrol_array = np.array([lunch1, smoking1])
+    # data_frame = pd.DataFrame(data=patrol_array, columns=['종류', '월요일', '화요일', '수요일', '목요일', '금요일'], index=['점심', '흡연'])
 
-    lunch1 = ['점심']
-    smoking1 = ['흡연']
+    lunch1 = pd.read_sql('select weekday, student1, student2 from patrol where type="점심1"', sql.connect("app/student.db"))
+    lunch2 = pd.read_sql('select weekday, student1, student2 from patrol where type="점심2"', sql.connect("app/student.db"))
+    smoking1 = pd.read_sql('select weekday, student1, student2 from patrol where type="흡연1"',
+                           sql.connect("app/student.db"))
+    smoking2 = pd.read_sql('select weekday, student1, student2 from patrol where type="흡연2"',
+                           sql.connect("app/student.db"))
 
-    for i in data:
-        if i.type == "점심":
-            lunch1.append(i.name)
-        else:
-            smoking1.append(i.name)
-
-    patrol_array = np.array([lunch1, smoking1])
-    data_frame = pd.DataFrame(data=patrol_array, columns=['종류', '월요일', '화요일', '수요일', '목요일', '금요일'], index=['점심', '흡연'])
-
-    return render_template('index.html', df=data_frame)
+    return render_template('index.html',
+                           lunch1=lunch1,
+                           lunch2=lunch2,
+                           smoking1=smoking1,
+                           smoking2=smoking2)
